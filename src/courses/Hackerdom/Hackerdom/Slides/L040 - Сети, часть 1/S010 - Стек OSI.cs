1c237e0737234f4d97ec9c1fbd980540d22da3f4
KiskachiMaria <kiskachi.maria@yandex.ru>
2015-10-15 15:47:51 +0500

    Правки от Андрея по лекции Сети 1

--- src/courses/Hackerdom/Hackerdom/Slides/L040 - Сети, часть 1/S010 - Стек OSI.cs
+++ src/courses/Hackerdom/Hackerdom/Slides/L040 - Сети, часть 1/S010 - Стек OSI.cs
@@ -3,8 +3,8 @@
 using uLearn;
 namespace Hackerdom 
 {
-    [Slide(@"Стэк OSI","ed50edd9-688d-4b94-bdc1-9f899880e4eb")]
-    public class Стэк_OSI
+    [Slide(@"Стек OSI","ed50edd9-688d-4b94-bdc1-9f899880e4eb")]
+    public class Стек_OSI
         {
 		//#video UleXPiLMWU8
 
